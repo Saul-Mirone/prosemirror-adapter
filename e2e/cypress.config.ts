1b36e5fb35b29bdef46cf7569ed62f06536e38fd
Mirone Saul <Saul-Mirone@outlook.com>
2022-11-14 21:34:18 +0800

    test: 💍 add e2e test

---
+++ e2e/cypress.config.ts
@@ -0,0 +1,8 @@
+/* Copyright 2021, Prosemirror Adapter by Mirone. */
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+    e2e: {
+        supportFile: false,
+    },
+});
---
+++ e2e/cypress/e2e/vue/node-view.cy.ts
@@ -0,0 +1,37 @@
+/* Copyright 2021, Prosemirror Adapter by Mirone. */
+/* eslint-disable promise/catch-or-return */
+
+Cypress.config('baseUrl', `http://localhost:${Cypress.env('SERVER_PORT')}`);
+
+beforeEach(() => {
+    cy.visit('/vue/');
+});
+
+it('heading node view render', () => {
+    cy.get('.editor')
+        .get('[data-node-view-root="true"]')
+        .get('h3')
+        .get('[data-node-view-content="true"]')
+        .contains('Hello ProseMirror');
+});
+
+it('paragraph node view render', () => {
+    cy.get('.editor')
+        .get('blockquote')
+        .get('[data-node-view-root="true"]')
+        .get('p')
+        .get('[data-node-view-content="true"]')
+        .contains('This is editable text');
+});
+
+it('heading node view update', () => {
+    const isMac = Cypress.platform === 'darwin';
+
+    const key = isMac ? '{cmd+[}' : '{ctrl+[}';
+
+    cy.get('.editor').type(`{selectAll}{leftArrow}${key}`, { delay: 500 });
+    cy.get('.editor').get('[data-node-view-root="true"]').get('h4').should('exist');
+
+    cy.get('.editor').type(`{selectAll}{leftArrow}${key}`, { delay: 500 });
+    cy.get('.editor').get('[data-node-view-root="true"]').get('h5').should('exist');
+});
