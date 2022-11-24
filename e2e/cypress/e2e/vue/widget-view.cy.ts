eac5e1023d7a9c02d21a439f8a9154693a397666
Mirone Saul <Saul-Mirone@outlook.com>
2022-11-24 11:38:38 +0800

    test: 💍 add test for widget view

---
+++ e2e/cypress/e2e/vue/widget-view.cy.ts
@@ -0,0 +1,27 @@
+/* Copyright 2021, Prosemirror Adapter by Mirone. */
+
+Cypress.config('baseUrl', `http://localhost:${Cypress.env('SERVER_PORT')}`)
+
+beforeEach(() => {
+  cy.visit('/vue/')
+})
+
+it('Focus heading to render widget', () => {
+  cy.get('.editor')
+    .find('h3')
+    .click()
+
+  cy.get('i[data-widget-view-root="true"]')
+    .contains('###')
+
+  cy.get('.editor').click()
+
+  cy.get('i[data-widget-view-root="true"]').should('not.exist')
+
+  cy.get('.editor')
+    .find('h3')
+    .type('{enter}# Heading 1')
+
+  cy.get('i[data-widget-view-root="true"]')
+    .contains('#')
+})
