a57a669f8224c18afefc70ff3576e84fddb0afdd
ocavue <ocavue@gmail.com>
2024-11-28 20:26:22 +0800

    bug fix

--- e2e/src/react/components/Link.tsx
+++ e2e/src/react/components/Link.tsx
@@ -8,20 +8,9 @@ export function Link() {
   const href = mark.attrs.href as string
 
   return (
-    <span>
-      <a
-        href={href}
-        onPointerEnter={() => setCount(count => count + 1)}
-        ref={contentRef}
-      >
-      </a>
-      <span style={{ opacity: 0.5 }}>
-        {' '}
-        (hover count:
-        {' '}
-        {count}
-        )
-      </span>
+    <span onPointerEnter={() => setCount(count => count + 1)}>
+      <a href={href} ref={contentRef}></a>
+      <span style={{ opacity: 0.5 }}>{` (hover count: ${count})`}</span>
     </span>
   )
 }
