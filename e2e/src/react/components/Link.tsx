f241c3cb054d75a92d1f94cdf2b38abbaf9c115a
ocavue <ocavue@gmail.com>
2024-11-28 20:24:06 +0800

    simplify example

--- e2e/src/react/components/Link.tsx
+++ e2e/src/react/components/Link.tsx
@@ -1,20 +1,5 @@
 import { useMarkViewContext } from '@prosemirror-adapter/react'
-import { useEffect, useState } from 'react'
-
-const linkCounter: Record<string, number> = {}
-const linkListeners: Record<string, Set<VoidFunction>> = {}
-
-function increaseLinkCount(link: string) {
-  linkCounter[link] ||= 0
-  linkCounter[link] += 1
-  linkListeners[link]?.forEach(fn => fn())
-}
-
-function addLinkListener(link: string, fn: VoidFunction): VoidFunction {
-  linkListeners[link] ||= new Set()
-  linkListeners[link].add(fn)
-  return () => linkListeners[link]?.delete(fn)
-}
+import { useState } from 'react'
 
 export function Link() {
   const { contentRef, mark } = useMarkViewContext()
@@ -22,32 +7,21 @@ export function Link() {
 
   const href = mark.attrs.href as string
 
-  useEffect(() => {
-    const dispose = addLinkListener(href, () => {
-      setCount(linkCounter[href] || 0)
-    })
-    return dispose
-  }, [href])
-
-  // Simulate link count increasement
-  useEffect(() => {
-    const id = setInterval(() => {
-      increaseLinkCount(href)
-    }, Math.round(Math.random() * 2000 + 1000))
-    return () => clearInterval(id)
-  }, [href])
-
   return (
-    <a
-      href={href}
-    >
-      <span
+    <span>
+      <a
+        href={href}
+        onPointerEnter={() => setCount(count => count + 1)}
         ref={contentRef}
       >
-      </span>
-      <span>
+      </a>
+      <span style={{ opacity: 0.5 }}>
+        {' '}
+        (hover count:
+        {' '}
         {count}
+        )
       </span>
-    </a>
+    </span>
   )
 }
