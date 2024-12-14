823c8da242174de53227fbfd9e12b971640c06cd
ocavue <ocavue@gmail.com>
2024-12-14 18:40:25 +1100

    fix text cursor jumping

--- e2e/src/react/components/Link.tsx
+++ e2e/src/react/components/Link.tsx
@@ -8,9 +8,15 @@ export function Link() {
   const href = mark.attrs.href as string
 
   return (
-    <span onPointerEnter={() => setCount(count => count + 1)}>
+    <span
+      onPointerEnter={() => setCount(count => count + 1)}
+      style={{
+        // @ts-expect-error: CSS custom properties are not typed
+        '--hover-count': JSON.stringify(String(count)),
+      }}
+      className="link-with-hover-count"
+    >
       <a href={href} ref={contentRef}></a>
-      <span style={{ opacity: 0.5 }}>{` (hover count: ${count})`}</span>
     </span>
   )
 }
