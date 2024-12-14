8be4c62926907af2f7d17024c5b56ee2e81d4beb
ocavue <ocavue@gmail.com>
2024-12-14 20:24:23 +1100

    update test

--- e2e/tests/mark-view.spec.ts
+++ e2e/tests/mark-view.spec.ts
@@ -1,27 +1,38 @@
 import { expect, test } from '@playwright/test'
 import { testAll } from './helpers'
 
-testAll(() => {
-  test('link mark view render', async ({ page }) => {
-    const locator = page.locator('.editor [data-mark-view-root="true"] a [data-mark-view-content="true"]')
-    await expect(locator).toBeVisible()
-    await expect(locator).toContainText('links')
-  })
+testAll(
+  () => {
+    test('link mark view render', async ({ page }) => {
+      const locator = page.locator(
+        '.editor [data-mark-view-root="true"] a [data-mark-view-content="true"]',
+      )
+      await expect(locator).toBeVisible()
+      await expect(locator).toContainText('links')
+    })
 
-  test('link mark view update', async ({ page }) => {
-    const locator = page.locator('.editor [data-mark-view-root="true"]')
-    await expect(locator).toContainText('hover count: 0')
+    test('link mark view update', async ({ page }) => {
+      const locator = page.locator('.editor [data-mark-view-root="true"] a')
+      await expect(locator).toBeVisible()
 
-    // Hover the link mark view
-    await locator.hover()
-    await expect(locator).toContainText('hover count: 1')
+      async function getStyleColor(): Promise<string> {
+        return locator.evaluate(el =>
+          window.getComputedStyle(el).getPropertyValue('color'),
+        )
+      }
 
-    // Leave the link mark view by hovering over something else
-    await page.locator('.editor h3').hover()
-    await expect(locator).toContainText('hover count: 1')
-
-    // Hover the link mark view again
-    await locator.hover()
-    await expect(locator).toContainText('hover count: 2')
-  })
-}, /* TODO: remove this once we have implemented mark view for other frameworks */ ['react'])
+      // Expect the color to change at least 2 times
+      const colors = new Set<string>()
+      for (let i = 0; i < 100; i++) {
+        colors.add(await getStyleColor())
+        if (colors.size > 2) {
+          break
+        }
+        await page.waitForTimeout(100)
+      }
+      expect(colors.size).toBeGreaterThan(2)
+    })
+  },
+  /* TODO: remove this once we have implemented mark view for other frameworks */
+  ['react'],
+)
