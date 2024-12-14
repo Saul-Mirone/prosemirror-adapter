75337763728700063a50de53f2b948862b0cf656
ocavue <ocavue@gmail.com>
2024-12-14 22:28:41 +1100

    lit

--- packages/lit/src/Provider.ts
+++ packages/lit/src/Provider.ts
@@ -8,19 +8,24 @@ import { useLitPluginViewCreator } from './pluginView/useLitPluginViewCreator'
 import { ShallowLitElement } from './utils/ShallowLitElement'
 import { widgetViewFactoryKey } from './widgetView'
 import { useLitWidgetViewCreator } from './widgetView/useLitWidgetViewCreator'
+import { markViewFactoryKey } from './markView'
+import { useLitMarkViewCreator } from './markView/useLitMarkViewCreator'
 
 @customElement('prosemirror-adapter-provider')
 export class ProsemirrorAdapterProvider extends ShallowLitElement {
   createLitNodeView: ContextProvider<typeof nodeViewFactoryKey>
+  createLitMarkView: ContextProvider<typeof markViewFactoryKey>
   createLitPluginView: ContextProvider<typeof pluginViewFactoryKey>
   createLitWidgetView: ContextProvider<typeof widgetViewFactoryKey>
   constructor() {
     super()
     const { renderLitRenderer, removeLitRenderer } = useLitRenderer()
     const createLitNodeView = useLitNodeViewCreator(renderLitRenderer, removeLitRenderer)
+    const createLitMarkView = useLitMarkViewCreator(renderLitRenderer, removeLitRenderer)
     const createLitPluginView = useLitPluginViewCreator(renderLitRenderer, removeLitRenderer)
     const createLitWidgetView = useLitWidgetViewCreator(renderLitRenderer, removeLitRenderer)
     this.createLitNodeView = new ContextProvider(this, nodeViewFactoryKey, createLitNodeView)
+    this.createLitMarkView = new ContextProvider(this, markViewFactoryKey, createLitMarkView)
     this.createLitPluginView = new ContextProvider(this, pluginViewFactoryKey, createLitPluginView)
     this.createLitWidgetView = new ContextProvider(this, widgetViewFactoryKey, createLitWidgetView)
   }
