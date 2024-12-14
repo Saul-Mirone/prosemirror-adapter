1ba0d4c86cd8b897a0b26bd5cab4e05bf90202fd
ocavue <ocavue@gmail.com>
2024-12-14 22:14:36 +1100

    svelte

--- packages/svelte/src/Provider.ts
+++ packages/svelte/src/Provider.ts
@@ -6,17 +6,22 @@ import { useSveltePluginViewCreator } from './pluginView/useSveltePluginViewCrea
 import { useSvelteRenderer } from './SvelteRenderer'
 import { widgetViewFactoryKey } from './widgetView'
 import { useSvelteWidgetViewCreator } from './widgetView/useSvelteWidgetViewCreator'
+import { markViewFactoryKey } from './markView'
+import { useSvelteMarkViewCreator } from './markView/useSvelteMarkViewCreator'
 
 export function useProsemirrorAdapterProvider() {
   const {
     renderSvelteRenderer,
     removeSvelteRenderer,
   } = useSvelteRenderer()
+
   const createSvelteNodeView = useSvelteNodeViewCreator(renderSvelteRenderer, removeSvelteRenderer)
+  const createSvelteMarkView = useSvelteMarkViewCreator(renderSvelteRenderer, removeSvelteRenderer)
   const createSveltePluginView = useSveltePluginViewCreator(renderSvelteRenderer, removeSvelteRenderer)
   const createSvelteWidgetView = useSvelteWidgetViewCreator(renderSvelteRenderer, removeSvelteRenderer)
 
   setContext(nodeViewFactoryKey, createSvelteNodeView)
+  setContext(markViewFactoryKey, createSvelteMarkView)
   setContext(pluginViewFactoryKey, createSveltePluginView)
   setContext(widgetViewFactoryKey, createSvelteWidgetView)
 }
